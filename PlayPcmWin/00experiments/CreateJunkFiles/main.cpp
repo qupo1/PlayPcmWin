e44fee397c8dad1952deb9284bec92c39d21ff16
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2021-06-10 22:02:58 +0000

    CreateJunkFiles project

--- PlayPcmWin/00experiments/CreateJunkFiles/main.cpp
+++ PlayPcmWin/00experiments/CreateJunkFiles/main.cpp
@@ -2,41 +2,39 @@
 #include <stdlib.h>
 #include <string>
 #include <stdint.h>
+#include <Windows.h>
+
+
+static const int FILES_PER_FOLDER = 1000;
+
 
 static void
 PrintUsage(void)
 {
-    printf("Usage: CreateJunkFiles dirPath fileNamePrefix [filesize in bytes < 2GB]\n");
+    printf("Usage: CreateJunkFiles dirPath [filesize in bytes]\n");
 }
 
+
 static bool
-CreateJunkFile(const wchar_t *dirPath, const wchar_t *fileNamePrefix, int idx, int fileSz)
+CreateJunkFile1(const wchar_t *dirPath, int idx, uint64_t fileSz, const uint8_t *buf)
 {
     bool br = false;
     FILE *fp = nullptr;
-    uint8_t *p = nullptr;
-    wchar_t s[256] = {};
+    wchar_t s[MAX_PATH] = {};
     size_t sz = 0;
     
-    swprintf_s(s, L"%s/%s%08d.bin", dirPath, fileNamePrefix, idx);
+    swprintf_s(s, L"%s/%d.bin", dirPath, idx);
 
     auto ercd = _wfopen_s(&fp, s, L"wb");
     if (ercd != 0) {
-        printf("Error: file open for write failed\n");
+        printf("Error: file open for write failed %S\n", s);
         return false;
     }
 
-    printf("Created %S\n", s);
+    printf("%S        \n", s);
 
-    p = (uint8_t*)malloc(fileSz);
-    if (p == nullptr) {
-        printf("Error: malloc failed\n");
-        goto end;
-    }
-    memset(p, 0x55, fileSz);
-
-    sz = fwrite(p, 1, fileSz, fp);
-    printf("  fwrite %d bytes\n", sz);
+    sz = fwrite(buf, 1, fileSz, fp);
+    //printf("  fwrite %d bytes\n", sz);
     if (sz != fileSz) {
         goto end;
     }
@@ -44,9 +42,6 @@ CreateJunkFile(const wchar_t *dirPath, const wchar_t *fileNamePrefix, int idx, i
     br = true;
 
 end:
-    free(p);
-    p = nullptr;
-
     if (fp != nullptr) {
         fclose(fp);
         fp = nullptr;
@@ -55,27 +50,59 @@ end:
     return br;
 }
 
+
+static void
+CreateJunkFiles(const wchar_t *parentDirPath, uint64_t fileSz, const uint8_t *buf)
+{
+    wchar_t s[MAX_PATH] = {};
+
+    for (int dir1Idx = 0;; ++dir1Idx) {
+        // 1層目のフォルダー作成。
+        swprintf_s(s, L"%s/%d", parentDirPath, dir1Idx);
+        CreateDirectoryW(s, nullptr);
+
+        for (int dir2Idx = 0; dir2Idx < FILES_PER_FOLDER; ++dir2Idx) {
+            // 2層目のフォルダー作成。
+            swprintf_s(s, L"%s/%d/%d", parentDirPath, dir1Idx, dir2Idx);
+            CreateDirectoryW(s, nullptr);
+
+            for (int i = 0; i < FILES_PER_FOLDER; ++i) {
+
+                bool b = CreateJunkFile1(s, i, fileSz, buf);
+                if (!b) {
+                    return;
+                }
+            }
+        }
+    }
+}
+
+
 int wmain(int argc, wchar_t *argv[])
 {
+    uint8_t *buf = nullptr;
+    uint64_t fileSz = 1024 * 1024;
+
     if (argc < 3) {
         PrintUsage();
         return 1;
     }
 
-    int fileSz = 1024 * 1024;
-    if (argc == 4) {
-        fileSz = wcstol(argv[3], nullptr, 10);
-    }
-
     const wchar_t *dirPath = argv[1];
-    const wchar_t *fileNamePrefix = argv[2];
 
-    for (int i=0; ; ++i) {
-        bool br = CreateJunkFile(dirPath, fileNamePrefix, i, fileSz);
-        if (!br) {
-            break;
-        }
+    fileSz = _wcstoui64(argv[2], nullptr, 10);
+    buf = (uint8_t*)malloc(fileSz);
+    if (buf == nullptr) {
+        printf("Error: malloc failed\n");
+        goto end;
     }
+    memset(buf, 0x55, fileSz);
+
+    CreateJunkFiles(dirPath, fileSz, buf);
+
+end:
+    free(buf);
+    buf = nullptr;
 
     return 0;
 }
