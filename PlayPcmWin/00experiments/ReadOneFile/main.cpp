3a0555ef52f6899d08c307c0f8987743d8a08fb7
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2021-06-11 08:43:48 +0000

    ReadOneFile project

---
+++ PlayPcmWin/00experiments/ReadOneFile/main.cpp
@@ -0,0 +1,118 @@
+#include <Windows.h>
+#include <stdio.h>
+#include <stdint.h>
+
+static void
+PrintUsage(void)
+{
+    printf("Usage: ReadOneFile filename\n");
+}
+
+static HRESULT
+ReadOneFile(const wchar_t *path, uint8_t *buf, const DWORD bufBytes, int64_t &fileBytes)
+{
+    HRESULT hr = S_OK;
+    HANDLE fh = INVALID_HANDLE_VALUE;
+    DWORD readBytes = 0;
+    int count = 0;
+    fileBytes = 0;
+
+    fh = CreateFile(path,
+            GENERIC_READ, //< dwDesiredAccess
+            FILE_SHARE_READ, //< dwShareMode
+            nullptr, //< lpSecurityAttributes
+            OPEN_EXISTING, //< dwCreationDisposition 
+            FILE_ATTRIBUTE_NORMAL | FILE_FLAG_NO_BUFFERING, //< dwFlagsAndAttributes
+            nullptr //< hTemplateFile
+            );
+
+    if (INVALID_HANDLE_VALUE == fh) {
+        hr = GetLastError();
+        printf("Error: ReadOneFile file open failed %d\n", hr);
+        goto end;
+    }
+
+    while (true) {
+        BOOL b = ReadFile(fh,
+                buf,
+                bufBytes,
+                &readBytes,
+                nullptr);
+
+        // printf("%d\n", count++);
+
+        if (b == 0) {
+            hr = GetLastError();
+        }
+        if (b == 0 || FAILED(hr) || readBytes == 0) {
+            goto end;
+        }
+
+        fileBytes += readBytes;
+    }
+
+end:
+    if (fh != INVALID_HANDLE_VALUE) {
+        CloseHandle(fh);
+        fh = INVALID_HANDLE_VALUE;
+    }
+
+    return hr;
+}
+
+
+int
+wmain(int argc, wchar_t *argv[])
+{
+    HRESULT hr = E_FAIL;
+    LARGE_INTEGER freqTick;
+    LARGE_INTEGER startTick;
+    LARGE_INTEGER endTick;
+    double recipFreq;
+    uint8_t *buf = nullptr;
+    const DWORD bufBytes = 1024 * 1024;
+    int64_t fileBytes = 0;
+    double elapsedSec = 0;
+
+    if (argc < 2) {
+        PrintUsage();
+        return 1;
+    }
+
+    const wchar_t *path = argv[1];
+
+    buf = (uint8_t*)malloc(bufBytes);
+    if (buf == nullptr) {
+        printf("Error: memory exhausted\n");
+        goto end;
+    }
+    memset(buf, 0, bufBytes);
+
+    // パフォーマンスカウンタ。
+    QueryPerformanceFrequency(&freqTick);
+    recipFreq = 1.0 / freqTick.QuadPart;
+
+    // 計測開始。
+    QueryPerformanceCounter(&startTick);
+
+    hr = ReadOneFile(path, buf, bufBytes, fileBytes);
+
+    // 計測終了。
+    QueryPerformanceCounter(&endTick);
+
+    if (FAILED(hr)) {
+        printf("ReadOneFile Failed: %S\n", path);
+        goto end;
+    }
+
+    // 経過時間表示。
+    elapsedSec = ((double)(endTick.QuadPart - startTick.QuadPart)) * recipFreq;
+    printf("%f seconds. %f MB/s\n", elapsedSec, fileBytes / elapsedSec / 1000.0 / 1000.0);
+
+end:
+    free(buf);
+    buf = nullptr;
+
+    return SUCCEEDED(hr) ? 0 : 1;
+}
+
