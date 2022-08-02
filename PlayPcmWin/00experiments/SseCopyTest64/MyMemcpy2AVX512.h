77ffa76d6b70a673edd5ca911ea84e07a2be373d
yamamoto2002 <yamamoto2002@7aea3531-1b4f-4469-99c4-25ca468c163d>
2022-08-02 20:15:05 +0000

    fix comment error

--- PlayPcmWin/00experiments/SseCopyTest64/MyMemcpy2AVX512.h
+++ PlayPcmWin/00experiments/SseCopyTest64/MyMemcpy2AVX512.h
@@ -5,7 +5,7 @@
 // このファイルではなくMyMemcpy2.hをインクルードしてMyMemcpy2を呼んで下さい。
 
 
-/// @param dst must be 32-byte aligned
+/// @param dst must be 64-byte aligned
 /// @param src is not needed to be aligned
 /// @param bytes must be multiply of 128
 extern "C" void MyMemcpy2AVX512(uint8_t * dst, const uint8_t * src, int64_t bytes);
